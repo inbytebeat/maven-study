5b504a406972d580ac16c6f0f454eb0374d28876
inbytebeat <1515577847@qq.com>
2022-06-01 08:50:06 +0000

    update src/main/java/com/xu/Demo1.java. gitee test2

--- src/main/java/com/xu/Demo1.java
+++ src/main/java/com/xu/Demo1.java
@@ -21,6 +21,7 @@ public class Demo1
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gitee test");
+        System.out.println("gitee test2");
         return name;
     }
 }
